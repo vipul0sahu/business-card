2062b36637efdaab4829ac2347e04b1dc6d8fd53
Vipul Sahu <vipul0sahu@gmail.com>
2024-04-15 16:17:10 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,68 @@
+import "./App.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSquareGithub, faLinkedin, faBehance, faCodepen, faDribbble} from '@fortawesome/free-brands-svg-icons'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
+export default function App() {
+  return (
+    <div>
+    <div className="card">
+
+      <div className="row">
+        <img className="card__image" src= "src/assets/zulmaury-saavedra-unsplash.jpg" alt="women potrait" />
+      </div>
+
+        <div className="row">
+          <section className="card__first-section">
+            <div className="container">
+              <div className="row">
+                  <h1 className="card__first-section__heading">Laura Smith</h1>
+                  <h4 className="card__first-section__heading-two">Frontend Developer</h4>
+                  <h5 className="card__first-section__heading-three">laurasmith_website</h5>
+              </div>
+            </div>
+          </section>
+
+          <section className="card__second-section">
+            <button className="card__second-section--light"><FontAwesomeIcon icon={faEnvelope} /> Email</button>
+            <button className="card__second-section--dark"><FontAwesomeIcon icon={faLinkedin} /> Linkedin</button>
+          </section>
+
+          <section className="card__third-section">
+            <div className="container">
+              <div className="row">
+                <div>
+                  <h3 className="card__third-section__heading">About</h3>
+                  <p className="card__third-sectoin__title">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio
+                    impedit quam facilis, quos odio cumque dignissimos? Consequatur
+                  </p>
+                </div>
+                <div>
+                  <h3 className="card__third-section__heading">Interests</h3>
+                  <p className="card__third-sectoin__title">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate
+                    eveniet ullam nulla id quas reprehenderit iusto in minus. Atque
+                  </p>
+                </div>
+              </div>
+            </div>
+          </section>
+
+
+      </div>
+        </div>
+
+          <section className="card__fourth-section">
+            <div className="container">
+              <div className="row d-flex">
+                <FontAwesomeIcon icon={faSquareGithub} size="2x"/>
+                <FontAwesomeIcon icon={faBehance} size="2x"/>
+                <FontAwesomeIcon icon={faDribbble} size="2x"/>
+                <FontAwesomeIcon icon={faCodepen} size="2x"/>
+              </div>
+            </div>
+          </section>
+    </div>
+  );
+}
